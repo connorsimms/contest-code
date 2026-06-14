5ce0b10b270966f4913cc8e45c999c9764de738d
Connor Simms <connorvsimms@gmail.com>
2026-06-14 18:00:12 +0000

    Improved template

--- templates/basic.cpp
+++ templates/basic.cpp
@@ -1,10 +1,16 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-
 using ll = long long;
+using ull = unsigned long long;
+
+template <class T> using v = vector<T>;
+template <class T, class U> void chmax(T &l, const U &r) { l = l < r ? r : l; }
+template <class T, class U> void chmin(T &l, const U &r) { l = l > r ? r : l; }
+
+void solve() {}
 
 int main() {
-  ios_base::sync_with_stdio(false);
-  cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
 }
