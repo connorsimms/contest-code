acaa1d73e33ac2248ad61ce2138d456d479a80cd
Connor Simms <connorvsimms@gmail.com>
2026-06-26 17:39:27 +0000

    https://codeforces.com/contest/2203/problem/C

---
+++ codeforces/e187/c.cpp
@@ -0,0 +1,154 @@
+// clang-format off
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+
+using ll = long long;
+using ull = unsigned long long;
+using str = string;
+
+template <class T> constexpr T INF = numeric_limits<T>::max() / 2;
+template <class T> constexpr T NINF = numeric_limits<T>::min() / 2;
+
+template <class T> using V = vector<T>;
+template <class T> using VV = V<V<T>>;
+template <class T> using PQ = priority_queue<T>;
+template <class T> using PQG = priority_queue<T, vector<T>, greater<T>>;
+template <class T, class U = T> using P = pair<T, U>;
+template <class T> using OST = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+template <class T, class U> bool chmax(T &l, const U &r) { return (l < r) ? l = r, 1 : 0;}
+template <class T, class U> bool chmin(T &l, const U &r) { return (l > r) ? l = r, 1 : 0;}
+
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+inline ll rnd(ll a, ll b) { return std::uniform_int_distribution<ll>(a, b)(rng); }
+
+#ifdef DEBUG
+#include "debug.hpp"
+#else
+#define dbg(...)
+#define dbge(x) (x)
+#endif
+
+#ifdef LOCAL
+#include "test.hpp"
+#define RUN_TESTS() run_local_tests(argc, argv, gen, brute, true)
+#else
+#define RUN_TESTS()
+#endif
+
+// clang-format on
+
+void gen_case(int TC) {
+}
+
+void brute(int TC) {}
+
+void solve(int TC) {
+    ull s, m;
+    cin >> s >> m;
+
+    V<ull> c(64);
+
+    for (ull i = 63; i > 0; --i) {
+        if (s & (1ULL << i))
+            c[i] += 1;
+
+        if (!(m & (1ULL << i))) {
+            c[i - 1] += c[i] * 2;
+            c[i] = 0;
+        }
+    }
+
+    if (s & 1) {
+        ++c[0];
+    }
+
+    for (ull i = 63; i > 0; --i) {
+        if (!(m & (1ULL << i)) && c[i]) {
+            cout << "-1\n";
+            return;
+        }
+    }
+
+    if (!(m & 1) && c[0]) {
+        cout << "-1\n";
+        return;
+    }
+
+    dbg(c);
+
+    auto check = [&](ull mid) -> bool {
+        auto cc = c;
+
+        for (ull i = 63; i > 0; --i) {
+            if (cc[i] > mid) {
+                cc[i - 1] += (cc[i] - mid) * 2;
+                cc[i] = mid;
+            }
+
+            if (!(m & (1ULL << i))) {
+                cc[i - 1] += cc[i] * 2;
+                cc[i] = 0;
+            }
+        }
+
+        dbg(cc);
+
+        for (auto x : cc)
+            if (x > mid)
+                return false;
+
+        for (ull i = 63; i > 0; --i) {
+            if (!(m & (1ULL << i)) && cc[i]) {
+                return false;
+            }
+        }
+
+        if (!(m & 1) && cc[0]) {
+            return false;
+        }
+
+        return true;
+    };
+
+    ull l = 0, r = s;
+
+    ull ans = s;
+
+    while (l <= r) {
+        auto mid = (r - l) / 2 + l;
+
+        dbg(l, mid, r);
+
+        if (check(mid)) {
+            r = mid - 1;
+            chmin(ans, mid);
+        } else
+            l = mid + 1;
+    }
+
+    cout << ans << '\n';
+}
+
+void gen(int seed) {
+    rng.seed(seed);
+    int tc = rnd(1, 10);
+    cout << tc << '\n';
+    for (int i = 1; i <= tc; ++i)
+        gen_case(i);
+}
+
+int main(int argc, char *argv[]) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    RUN_TESTS();
+
+    int TC;
+    cin >> TC;
+    for (int i = 1; i <= TC; ++i)
+        solve(i);
+}
