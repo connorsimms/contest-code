95e52c6bfb2c074b5370a071a9f9dd1a9336e697
Connor Simms <connorvsimms@gmail.com>
2026-06-30 16:53:05 +0000

    https://codeforces.com/contest/2241/problem/E

---
+++ codeforces/1107/e.cpp
@@ -0,0 +1,134 @@
+// clang-format off
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+
+using ll = long long;
+using ull = unsigned long long;
+using str = string;
+
+template <class T> constexpr T INF = numeric_limits<T>::max() / 2;
+template <class T> constexpr T NINF = numeric_limits<T>::min() / 2;
+
+template <class T> using V = vector<T>;
+template <class T> using VV = V<V<T>>;
+template <class T> using PQ = priority_queue<T>;
+template <class T> using PQG = priority_queue<T, vector<T>, greater<T>>;
+template <class T, class U = T> using P = pair<T, U>;
+template <class T> using OST = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+template <class T, class U> bool chmax(T &l, const U &r) { return (l < r) ? l = r, 1 : 0;}
+template <class T, class U> bool chmin(T &l, const U &r) { return (l > r) ? l = r, 1 : 0;}
+
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+inline ll rnd(ll a, ll b) { return std::uniform_int_distribution<ll>(a, b)(rng); }
+
+#ifdef DEBUG
+#include "debug.hpp"
+#else
+#define dbg(...)
+#define dbge(x) (x)
+#endif
+
+#ifdef LOCAL
+#include "test.hpp"
+#define RUN_TESTS() run_local_tests(argc, argv, gen, brute, true)
+#else
+#define RUN_TESTS()
+#endif
+
+// clang-format on
+
+void gen_case(int TC) {}
+
+void brute(int TC) {}
+
+V<bool> isSquare(1'000'001);
+
+void find_sq() {
+    for (int i = 0; i * i < isSquare.size(); ++i)
+        isSquare[i * i] = true;
+}
+
+void solve(int TC) {
+    int n;
+    cin >> n;
+    V<int> w(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> w[i];
+    }
+
+    VV<int> adj(n);
+    for (int i = 0; i < n - 1; ++i) {
+        int u, v;
+        cin >> u >> v;
+        --u;
+        --v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    V<int> par(n);
+    V<int> size(n, 1);
+
+    auto bfs = [&](int pre, int cur, auto &&self) -> int {
+        par[cur] = pre;
+        for (auto next : adj[cur]) {
+            if (next != pre)
+                size[cur] += self(cur, next, self);
+        }
+        return size[cur];
+    };
+
+    bfs(-1, 0, bfs);
+
+    dbg(size);
+
+    ll ans = 0;
+    for (int i = 0; i < n; ++i) {
+        if (isSquare[w[i]]) {
+            ll a{}, b{}, c{};
+
+            for (auto next : adj[i]) {
+                ll d = 0;
+                if (next == par[i]) {
+                    d = n - size[i];
+                } else {
+                    d = size[next];
+                }
+
+                c += b * d;
+                b += a * d;
+                a += d;
+            }
+
+            ans += b + c;
+        }
+    }
+
+    cout << ans << '\n';
+}
+
+void gen(int seed) {
+    rng.seed(seed);
+    int tc = rnd(1, 10);
+    cout << tc << '\n';
+    for (int i = 1; i <= tc; ++i)
+        gen_case(i);
+}
+
+int main(int argc, char *argv[]) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    find_sq();
+
+    RUN_TESTS();
+
+    int TC;
+    cin >> TC;
+    for (int i = 1; i <= TC; ++i)
+        solve(i);
+}
