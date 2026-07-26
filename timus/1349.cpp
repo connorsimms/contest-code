32e07e11016e1f8d1426ca0ae701feb0d5d803be
Connor Simms <connorvsimms@gmail.com>
2026-07-26 16:39:56 +0000

    https://acm.timus.ru/problem.aspx?space=1&num=1349

---
+++ timus/1349.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    if (n == 1)
+        cout << "1 2 3\n";
+    else if (n == 2)
+        cout << "3 4 5\n";
+    else
+        cout << "-1\n";
+}
